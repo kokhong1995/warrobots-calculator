93dd7988b3d2d66415b58238849bd789ab6b028f
KH Hean <40062461+kokhong1995@users.noreply.github.com>
2026-08-17 16:27:11 +0800

    Refactor syncData and reset functions for clarity

--- leaderboard/js/stunning-drones.js
+++ leaderboard/js/stunning-drones.js
@@ -1,25 +1,3 @@
-function toggleSection(event) {
-    var sectionHeaders = document.getElementsByClassName("section-header-button");
-    let sectionContentId;
-    let i;
-    let sectionContent;
-
-    for (i = 0; i < sectionHeaders.length; i++) {
-        sectionContentId = sectionHeaders[i].getAttribute('data-section-content').substring(1);
-        sectionContent = document.getElementById(sectionContentId);
-        console.log(sectionContentId);
-
-        if (sectionHeaders[i].id === event.id) {
-            sectionContent.classList.add('visible');
-            sectionContent.parentElement.classList.add('active');
-        }
-        else {
-            sectionContent.classList.remove('visible');
-            sectionContent.parentElement.classList.remove('active');
-        }
-    }
-}
-
 function toInt(value) {
     return parseInt(value) | 0;
 }
@@ -55,10 +33,6 @@ function applyDiscount(discountPercentage, value) {
 }
 
 function syncData() {
-    let intInitialPoints = 0, intDiscountPercentage = 0, intBattlesToWinPoints = 0;
-    let intQuantity = 0, intMicrochips = 0, intUpgradeTokens = 0, intPoints = 0;
-    let intTotalQuantity = 0, intTotalMicrochipCost = 0, intTotalUpgradeTokenCost = 0, intTotalPoints = 0;
-    let i;
     const inputDiscountPercentage = document.getElementById('inputDiscountPercentage');
     const inputInitialPoints = document.getElementById('inputInitialPoints');
     const inputBattlesToWin = document.getElementById('inputBattlesToWin');
@@ -70,6 +44,10 @@ function syncData() {
     const totalMicrochipCost = document.getElementById('totalMicrochipsCost');
     const totalUpgradeTokenCost = document.getElementById('totalUpgradeTokensCost');
     const totalPoints = document.getElementById('totalPoints');
+    let intInitialPoints = 0, intDiscountPercentage = 0, intBattlesToWinPoints = 0;
+    let intQuantity = 0, intMicrochips = 0, intUpgradeTokens = 0, intPoints = 0;
+    let intTotalQuantity = 0, intTotalMicrochipCost = 0, intTotalUpgradeTokenCost = 0, intTotalPoints = 0;
+    let i;
 
     // Get absolute value of discount percentage.
     intDiscountPercentage = Math.abs(toInt(inputDiscountPercentage.value));
@@ -77,14 +55,14 @@ function syncData() {
     if (droneUpgrades.length != inputQuantities.length) {
         return;
     }
-    
+
     for (i = 0; i < droneUpgrades.length; i++) {
         // Calculate quantity, microchips, upgrade tokens, and points.
         intQuantity = toInt(inputQuantities[i].value);
         intMicrochips = applyDiscount(intDiscountPercentage, droneUpgrades[i].microchips * intQuantity);
         intUpgradeTokens = droneUpgrades[i].upgradeTokens * intQuantity;
-        intPoints = droneUpgrades[i].leaderboardPoints * intQuantity;
-        
+        intPoints = droneUpgrades[i].points * intQuantity;
+
         // Set microchips, upgrade tokens, and  points.
         microchips[i].textContent = intMicrochips;
         upgradeTokens[i].textContent = intUpgradeTokens;
@@ -100,7 +78,7 @@ function syncData() {
     intInitialPoints = toInt(inputInitialPoints.value);
     intBattlesToWinPoints = toInt(inputBattlesToWin.value) * 10;
     intTotalPoints += intInitialPoints + intBattlesToWinPoints;
-    
+
     // Set total quantity, total microchip cost, total upgrade token cost and total points.
     totalQuantity.textContent = intTotalQuantity;
     totalMicrochipCost.textContent = intTotalMicrochipCost;
@@ -108,17 +86,21 @@ function syncData() {
     totalPoints.textContent = intTotalPoints;
 }
 
-function resetAll() {
+function resetModifier() {
     const inputDiscountPercentage = document.getElementById('inputDiscountPercentage');
     const inputInitialPoints = document.getElementById('inputInitialPoints');
     const inputBattlesToWin = document.getElementById('inputBattlesToWin');
-    const inputQuantities = document.querySelectorAll(".quantities");
 
     inputDiscountPercentage.value = 0;
     inputInitialPoints.value = 0;
     inputBattlesToWin.value = 0;
-    inputQuantities.forEach(input => input.value = 0);
+    syncData();
+}
 
+function resetDroneUpgrades() {
+    const inputQuantities = document.querySelectorAll(".quantities");
+
+    inputQuantities.forEach(input => input.value = 0);
     syncData();
 }
 
@@ -127,82 +109,82 @@ const droneUpgrades = [
     //     level: 1,
     //     microchips: 0,
     //     upgradeTokens: 0,
-    //     leaderboardPoints: 0
+    //     points: 0
     // },
     {
         level: 2,
         microchips: 40,
         upgradeTokens: 0,
-        leaderboardPoints: 120
+        points: 120
     },
     {
         level: 3,
         microchips: 60,
         upgradeTokens: 0,
-        leaderboardPoints: 180
+        points: 180
     },
     {
         level: 4,
         microchips: 90,
         upgradeTokens: 0,
-        leaderboardPoints: 270
+        points: 270
     },
     {
         level: 5,
         microchips: 100,
         upgradeTokens: 0,
-        leaderboardPoints: 300
+        points: 300
     },
     {
         level: 6,
         microchips: 100,
         upgradeTokens: 0,
-        leaderboardPoints: 300
+        points: 300
     },
     {
         level: 7,
         microchips: 100,
         upgradeTokens: 0,
-        leaderboardPoints: 300
+        points: 300
     },
     {
         level: 8,
         microchips: 120,
         upgradeTokens: 0,
-        leaderboardPoints: 360
+        points: 360
     },
     {
         level: 9,
         microchips: 120,
         upgradeTokens: 0,
-        leaderboardPoints: 360
+        points: 360
     },
     {
         level: 10,
         microchips: 130,
         upgradeTokens: 0,
-        leaderboardPoints: 390
+        points: 390
     },
     {
         level: 11,
         microchips: 140,
         upgradeTokens: 0,
-        leaderboardPoints: 420
+        points: 420
     },
     {
         level: 12,
         microchips: 0,
         upgradeTokens: 1,
-        leaderboardPoints: 600
+        points: 600
     }
 ];
 
-var droneUpgradeContainerInnerHTML = '';
-var droneUpgradeContainer = document.getElementById('droneUpgradeContainer');
-var i;
+const droneUpgradeContainer = document.getElementById('droneUpgradeContainer');
+let droneUpgradeContainerInnerHTML = '';
+let i;
 
 for (i = 0; i < droneUpgrades.length; i++) {
-    droneUpgradeContainerInnerHTML += '<div class="col-sm-6 col-lg-4 col-xl-3">' +
+    droneUpgradeContainerInnerHTML += '<div class="col-md-6 col-xxl-4">' +
         '<div class="item">' + '<div class="row align-items-center">' +
         '<div class="col">' + '<div class="item-title">Level&nbsp;' + droneUpgrades[i].level + '</div>' +
         '</div>' +
