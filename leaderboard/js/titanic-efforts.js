7eb4fd56c44f86ea689435125eb2fdaaf7b057af
kokhong1995 <kokhong.hean@gmail.com>
2026-09-04 22:06:38 +0800

    Renamed Gold to Gold (Instant Upgrade) to clarify that it is only used to skip Upgrade Duration, not as a requirement to unlock the upgrades themselves.

--- leaderboard/js/titanic-efforts.js
+++ leaderboard/js/titanic-efforts.js
@@ -1,6 +1,6 @@
-import { toInt, thousandSeperator, getStrAmount } from '/warrobots-calculator/js/data-helper.js?v=1.12.1';
-import { resetInputs, updateNumberInput } from '/warrobots-calculator/js/input-helper.js?v=1.12.1';
-import { applyUpgradeDiscountPercentage, calculateTotalTitanDeployPoints } from '/warrobots-calculator/js/modifier-helper.js?v=1.12.1';
+import { toInt, thousandSeperator, getStrAmount } from '/warrobots-calculator/js/data-helper.js?v=1.12.2';
+import { resetInputs, updateNumberInput } from '/warrobots-calculator/js/input-helper.js?v=1.12.2';
+import { applyUpgradeDiscountPercentage, calculateTotalTitanDeployPoints } from '/warrobots-calculator/js/modifier-helper.js?v=1.12.2';
 
 function updateInputValue(eventType, inputId) {
     updateNumberInput(eventType, inputId, syncData);
